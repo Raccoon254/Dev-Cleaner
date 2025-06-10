4059c3ff475c6f5ff1a4f1e5aff817bca6147edf
Steve Tom <tomsteve187@gmail.com>
2025-06-10 19:08:11 +0300

    More icons and project detail view init

--- src/main/java/com/kentom/devcleaner/model/Project.java
+++ src/main/java/com/kentom/devcleaner/model/Project.java
@@ -1,36 +1,77 @@
 package com.kentom.devcleaner.model;
 
 import javafx.scene.image.Image;
+
+import java.io.*;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
-public class Project {
+public class Project implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     private final String name;
-    private final Path path;
+    private final String pathString; // Store path as String for serialization
     private final ProjectType type;
-    private final Image icon;
-    private final List<Path> cleanableItems = new ArrayList<>();
+    private final LocalDateTime dateCreated;
+    private final List<String> cleanableItemsPaths = new ArrayList<>();
     private long sizeOfCleanableItems = 0;
+
+    private transient Image icon; // Exclude from serialization
 
     public Project(Path path, ProjectType type) {
         this.name = path.getFileName().toString();
-        this.path = path;
+        this.pathString = path.toAbsolutePath().toString();
         this.type = type;
-        this.icon = new Image(getClass().getResourceAsStream("/com/kentom/devcleaner/icons/" + type.iconName));
+        this.dateCreated = LocalDateTime.now();
+        loadIcon();
     }
 
     // Getters
     public String getName() { return name; }
-    public Path getPath() { return path; }
+    public Path getPath() { return Paths.get(pathString); }
     public ProjectType getType() { return type; }
-    public Image getIcon() { return icon; }
-    public List<Path> getCleanableItems() { return cleanableItems; }
+    public Image getIcon() {
+        if (icon == null) {
+            loadIcon();
+        }
+        return icon;
+    }
+    public List<Path> getCleanableItems() {
+        List<Path> paths = new ArrayList<>();
+        for (String p : cleanableItemsPaths) {
+            paths.add(Paths.get(p));
+        }
+        return paths;
+    }
     public long getSizeOfCleanableItems() { return sizeOfCleanableItems; }
+    public LocalDateTime getDateCreated() { return dateCreated; }
 
     // Public methods
     public void addCleanableItem(Path item, long size) {
-        this.cleanableItems.add(item);
+        this.cleanableItemsPaths.add(item.toAbsolutePath().toString());
         this.sizeOfCleanableItems += size;
+    }
+
+    private void loadIcon() {
+        try {
+            this.icon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/com/kentom/devcleaner/icons/" + type.iconName)));
+        } catch (Exception e) {
+            this.icon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/com/kentom/devcleaner/icons/default.png")));
+        }
+    }
+
+    @Serial
+    private void writeObject(ObjectOutputStream oos) throws IOException {
+        oos.defaultWriteObject();
+    }
+
+    @Serial
+    private void readObject(ObjectInputStream ois) throws ClassNotFoundException, IOException {
+        ois.defaultReadObject();
+        loadIcon();
     }
 }

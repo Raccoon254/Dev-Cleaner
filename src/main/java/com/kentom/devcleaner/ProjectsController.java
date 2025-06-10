6113dca7f798411b4e81eebdc6f37dada63f3ee6
Steve Tom <tomsteve187@gmail.com>
2025-06-10 23:06:23 +0300

    Fix Long Project names

--- src/main/java/com/kentom/devcleaner/ProjectsController.java
+++ src/main/java/com/kentom/devcleaner/ProjectsController.java
@@ -115,7 +115,7 @@ public class ProjectsController {
         // Emsure the name is like not more than 2 words
         String[] words = formattedName.toString().split(" ");
         if (words.length > 2) {
-            formattedName = new StringBuilder(words[0] + " " + words[1]);
+            formattedName = new StringBuilder(words[0] + " " + words[1] + "...");
         }
 
         return formattedName.toString();

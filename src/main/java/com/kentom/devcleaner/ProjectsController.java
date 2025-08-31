9ca4a9e3f75edf451a2fd7bfc5e4a20169cf7750
Steve Tom <tomsteve187@gmail.com>
2025-08-31 13:15:41 +0300

    Init copy path project -- details

--- src/main/java/com/kentom/devcleaner/ProjectsController.java
+++ src/main/java/com/kentom/devcleaner/ProjectsController.java
@@ -9,6 +9,9 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.TilePane;
@@ -16,12 +19,15 @@ import javafx.scene.layout.VBox;
 import javafx.util.Duration;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class ProjectsController {
 
     @FXML private TilePane projectTilePane;
     @FXML private VBox emptyStateBox;
+    @FXML private Button resetButton;
 
 
     private MainController mainController;
